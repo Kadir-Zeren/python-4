44848b99aed7535a24366ecb4bc04be7b7e1eb16
kadir-zeren <k4dzeren@gmail.com>
2024-01-23 13:31:44 +0300

    slice

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,24 @@
-print("Hello World")
+fruit = "Orange"
+
+print("Word:",fruit)
+
+print("First Letter:",fruit[0])
+
+print(fruit[1:4])
+
+print(fruit[:])
+
+print(fruit[:5])
+
+
+text = "abcdefghi"
+
+print(text[1:8])
+
+print(text[1:8:2])
+
+print(text[1::3])
+
+print(text[::2])
+
+print(text[5:1:-1])
